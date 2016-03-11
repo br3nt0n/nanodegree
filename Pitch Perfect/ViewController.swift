49f95b4232e2c826f2a0e9e537adcbbb4fec1f8c
Brenton Currie <brenton.currie@thecloudhub.com>
2016-03-11 20:08:17 +1100

    Started refactoring for project

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -9,20 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var recordingInProgress: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func recordAudio(sender: UIButton){
-        recordingInProgress.hidden = false
-    }
+  @IBOutlet weak var recordingInProgress: UILabel!
+  @IBOutlet weak var stopButton: UIButton!
+  @IBOutlet weak var recordButton: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    stopButton.hidden = true
+    recordButton.enabled = true
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func recordAudio(sender: UIButton){
+    recordButton.enabled = false
+    recordingInProgress.hidden = false
+    stopButton.hidden = false
+  }
+  
+  @IBAction func stopRecordingAudio(sender: UIButton){
+    recordingInProgress.hidden = true
+    recordButton.enabled = true
+    stopButton.hidden = true
+    performSegueWithIdentifier("showPlaySoundsView", sender: nil)
+  }
 }
 
